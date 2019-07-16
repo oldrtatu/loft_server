ce5a84ea13a0824c932464da2e3ed34627b360b6
rtatu <rohit.tatu896@gmail.com>
2019-07-16 18:14:40 +0530

    Added Error handling

--- routes/archive/service.js
+++ routes/archive/service.js
@@ -1,26 +1,37 @@
 const express = require('express')
 const router = express.Router()
 const model = require('../../models')
-
+const logger = require('../../logger/logger')
+const util = require('util')
+const Error = require('../../utils/Error')
 
 // get all services
 router.get('/', async (req, res, next) => {
     let services = await model.service.findAll({}).catch(err => {
-        res.status(400).json(err)
+        let response = Error.SequelizeErrorFormat(err)
+        res.status(400).send(response)
     })
     res.status(200).json(services)
 })
 
 // create a service
 router.post('/', async (req, res, next) => {
+
     let service = await model.service.create({
         name: req.body.name,
         textField: req.body.textField,
         associationWith: req.body.associationWith,
         status: req.body.status
-    }).catch(err => res.status(400).json(err))
+    }).catch(err => {
+        let response = Error.SequelizeErrorFormat(err)
+        res.status(400).send(response)
+    })
 
-    res.status(201).json(service)
+    res.status(201).json({
+        "CODE" : "ADD_SUCC",
+        response : service
+    })
+
 })
 
 
@@ -36,24 +47,54 @@ router.put('/', async (req, res, next) => {
         where: {
             id: req.body.id
         }
+    }).catch(err => {
+        let response = Error.SequelizeErrorFormat(err)
+        res.status(400).send(response)
     })
 
-    console.log(service)
+    if(service[0] === 1) {
+        res.status(200).json({
+            'code' : "UPDATE_SUCC",
+            response : req.body
+        })
+    } else {
+        res.status(200).json({
+            'code' : "UPDATE_FAIL",
+            'message' : 'ID doesn\'t exist'
+        })
+    }
 
-    res.status(400).json(service)
 })
 
 
 // delete a service
 router.delete('/', async (req, res, next) => {
+
     let service = await model.service.destroy({
         where: { id: req.body.id }
-    }).catch(err => console.log(err))
+    }).catch(err => {
+        let response = Error.SequelizeErrorFormat(err)
+        res.status(400).send(response)
+    })
 
     console.log(service)
 
-    res.status(400).json(d)
+    if(service === 1) {
+        res.status(200).json({
+            'code' : "DELETE_SUCC",
+            'response' : {
+                id : req.body.id
+            }
+        })
+    } else {
+        res.status(200).json({
+            'code' : "DELETE_FAIL",
+            'message' : 'ID doesn\'t exist'
+        })
+    }
 
 })
+
+
 
 module.exports = router
---
+++ utils/Error.js
@@ -0,0 +1,56 @@
+const Error = {
+
+    formatError : function(err){
+        // get all the keys of error object
+        let keys = Object.keys(err)
+
+        let data = {}
+
+        // get all the values of coresponding keys
+        for (let key of keys) {
+            
+            // add the key value pair to data array
+            data[key] = err[key]
+        }
+
+        return data
+    },
+
+    SequelizeErrorFormat : function(err) {
+        let error = {
+
+        }
+
+        let ErrorObject = this.formatError(err)
+
+        // set the code of the error
+        if(ErrorObject.original) {
+            error['code'] = ErrorObject.original.code
+            error['message'] = ErrorObject.original.sqlMessage
+        }
+
+        if(ErrorObject.fields) {
+            error['fields'] = {...ErrorObject.fields}
+        }
+
+        return this.displayOnlyError(error)
+    },
+
+    displayOnlyError : function(errObj) {
+        switch(errObj.code) {
+            case "ER_NO_SUCH_TABLE":
+                return {
+                    name : "DatabaseError",
+                    code : errObj.code
+                }
+            default:
+                return {
+                    name : "DatabaseError",
+                    ...errObj
+                }
+        }
+    }
+
+}
+
+module.exports = Error
