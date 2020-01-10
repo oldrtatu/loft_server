0912e75670e434e4024abd0dcbe3125f499e7208
rtatu <rohit.tatu896@gmail.com>
2020-01-10 19:03:53 +0530

    corrected truck

--- models/Truck.js
+++ models/Truck.js
@@ -1,47 +1,38 @@
 module.exports = (sequelize, DataTypes) => {
-    let Truck = sequelize.define('truck', {
-        id : {
-            type : DataTypes.INTEGER(11),
-            primaryKey : true,
-            autoIncrement: true,
-            unique : true,
-            allowNull : false
-        },
-        unitNo : {
-            type : DataTypes.STRING,
-            allowNull : false,
-            unique : true,
-        },
-        truckType : {
-            type : DataTypes.STRING,
-            allowNull : true,
-        },
-        status : {
-            type : DataTypes.ENUM(["ACTIVE", "INACTIVE"]),
-            allowNull : false
-        },
-        notes: {
-            type: DataTypes.BLOB,
-            allowNull : true,
-            get(){
-                let data = this.getDataValue("notes");
-                return JSON.parse(data);
-            },
-
-            set(value){
-                this.setDataValue("notes", JSON.stringify(value))
-            }
-        }
-
-    })
-
-    Truck.associate = (model) => {
-        Truck.belongsTo(model.class, {as : "category"})
-        Truck.belongsTo(model.subsidiary, {as: "division"})
-        Truck.hasOne(model.truckRegistration, {
-            onDelete : "CASCADE"
-        })
+  let Truck = sequelize.define("truck", {
+    id: {
+      type: DataTypes.INTEGER(11),
+      primaryKey: true,
+      autoIncrement: true,
+      unique: true,
+      allowNull: false
+    },
+    unitNo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    truckType: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    status: {
+      type: DataTypes.ENUM(["ACTIVE", "INACTIVE"]),
+      allowNull: false
+    },
+    notes: {
+      type: DataTypes.JSON,
+      allowNull: true
     }
+  });
 
-    return Truck
-}
+  Truck.associate = model => {
+    Truck.belongsTo(model.class, { as: "category" });
+    Truck.belongsTo(model.subsidiary, { as: "division" });
+    Truck.hasOne(model.truckRegistration, {
+      onDelete: "CASCADE"
+    });
+  };
+
+  return Truck;
+};
